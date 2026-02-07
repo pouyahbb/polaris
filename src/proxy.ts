55d92abfb456c68b64c9470bb8804f2e879758e6
pouyahbb <pouyahobbi504@gmail.com>
2026-02-08 00:58:48 +0330

    Database setup

--- src/proxy.ts
+++ src/proxy.ts
@@ -1,6 +1,11 @@
-import { clerkMiddleware } from '@clerk/nextjs/server';
+import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server';
+
+// const isPublicRoute = createRouteMatcher([
+//   '/api/inngest(.*)',
+// ]);
 
 export default clerkMiddleware();
+
 
 export const config = {
   matcher: [
